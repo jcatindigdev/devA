b6c2956687d77e0a0be17108422b0e39d74d13a3
vanezamaediarios <vanezamaediarios@gmail.com>
2026-07-22 11:34:22 +0800

    fixed navbar

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import './assets/main.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
